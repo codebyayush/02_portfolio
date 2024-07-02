8212bb82217a783251efc5452d9576a15da1ac9a
codebyayush <ayushpal6939@gmail.com>
2024-07-02 18:36:35 +0530

    project live links added

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -24,26 +24,26 @@ const Projects = () => {
           </div>
           <div className="proj-div border-2 p-3 border-gray-800 flex flex-col justify-center items-center w-1/4 rounded-3xl m-2">
             <img
-              src="/chat.png"
-              alt="chat"
-              className="h-80 w-80 rounded-3xl p-3 "
-            />
-            <h1 className="proj-title font-medium text-3xl">Mailbox App</h1>
-            <div className="proj-buttons flex w-full justify-center">
-              <a href="" target="_blank"><button className="border-2 border-gray-900 h-12 m-4  w-32 text-center rounded-full hover:text-white hover:bg-zinc-700 transition duration-700">Live Demo</button></a>
-              <a href="https://github.com/codebyayush/08_mailbox_client_md" target="_blank"><button className="border-2 border-gray-900 h-12 m-4  w-32 text-center rounded-full hover:text-white hover:bg-zinc-700 transition duration-700">Github</button></a>
-            </div>
-          </div>
-          <div className="proj-div border-2 p-3 border-gray-800 flex flex-col justify-center items-center w-1/4 rounded-3xl m-2">
-            <img
               src="/expense.png"
               alt="expnse"
               className="h-80 w-80 rounded-3xl p-3 "
             />
             <h1 className="proj-title font-medium text-3xl">Expense Tracker App</h1>
             <div className="proj-buttons flex w-full justify-center">
-              <a href="" target="_blank"><button className="border-2 border-gray-900 h-12 m-4  w-32 text-center rounded-full hover:text-white hover:bg-zinc-700 transition duration-700">Live Demo</button></a>
+              <a href="https://expense-tracker-481f2.web.app/" target="_blank"><button className="border-2 border-gray-900 h-12 m-4  w-32 text-center rounded-full hover:text-white hover:bg-zinc-700 transition duration-700">Live Demo</button></a>
               <a href="https://github.com/codebyayush/04_expensetracker_md" target="_blank"><button className="border-2 border-gray-900 h-12 m-4  w-32 text-center rounded-full hover:text-white hover:bg-zinc-700 transition duration-700">Github</button></a>
+            </div>
+          </div>
+          <div className="proj-div border-2 p-3 border-gray-800 flex flex-col justify-center items-center w-1/4 rounded-3xl m-2">
+            <img
+              src="/chat.png"
+              alt="chat"
+              className="h-80 w-80 rounded-3xl p-3 "
+            />
+            <h1 className="proj-title font-medium text-3xl">Mailbox App</h1>
+            <div className="proj-buttons flex w-full justify-center">
+              <a href="https://mailbox-client-md.web.app/" target="_blank"><button className="border-2 border-gray-900 h-12 m-4  w-32 text-center rounded-full hover:text-white hover:bg-zinc-700 transition duration-700">Live Demo</button></a>
+              <a href="https://github.com/codebyayush/08_mailbox_client_md" target="_blank"><button className="border-2 border-gray-900 h-12 m-4  w-32 text-center rounded-full hover:text-white hover:bg-zinc-700 transition duration-700">Github</button></a>
             </div>
           </div>
         </div>
